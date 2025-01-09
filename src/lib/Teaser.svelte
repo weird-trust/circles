c9971b888795336c307cbc09a67e44bf88813922
Robert Burtzik <robert.burtzik@fork.de>
2025-01-09 16:43:19 +0100

    feat: adjust teaser-trigger

---
+++ src/lib/Teaser.svelte
@@ -0,0 +1,61 @@
+<script>
+  export let headline = "";
+  export let copy = "";
+  export let cta = "";
+</script>
+
+<div class="teaser-content">
+  <div class="headline">{headline}</div>
+  <div class="copy">{copy}</div>
+  <div class="cta">{cta}</div>
+</div>
+
+<style>
+  .teaser-content {
+    display: flex;
+    padding: var(--l, 32px);
+    flex-direction: column;
+    align-items: flex-start;
+    gap: var(--m, 16px);
+    align-self: stretch;
+    border-radius: var(--m, 16px);
+    background: var(--primary-bosch-trkis-50-markt-und-trends, #18837e);
+    color: #fff;
+    position: absolute;
+    top: 20px; /* Beispielposition - anpassen, um im sichtbaren Bereich zu sein */
+    left: 20px; /* Beispielposition - anpassen, um im sichtbaren Bereich zu sein */
+    z-index: 1000;
+    pointer-events: none;
+    transition: opacity 0.5s ease; /* Hinzufügen von Übergängen */
+  }
+
+  .headline {
+    color: #fff;
+    font-family: "Helvetica", sans-serif;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 32px;
+    letter-spacing: -0.24px;
+  }
+
+  .copy {
+    color: #fff;
+    font-family: "Helvetica", sans-serif;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 150%;
+  }
+
+  .cta {
+    color: #fff;
+    font-family: "Bosch Sans", sans-serif;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 150%;
+    letter-spacing: 0.56px;
+    text-transform: uppercase;
+  }
+</style>
--- src/routes/ScrollAnimation.svelte
+++ src/routes/ScrollAnimation.svelte
@@ -8,127 +8,153 @@
 
   onMount(() => {
     const path = document.querySelector("#line");
-    const circle = document.querySelector("#movingCircle");
+    const circle = document.querySelector("#movingCircle circle");
     const teaserContent = document.querySelector("#teaserContent");
-    const headline = teaserContent.querySelector(".teaser-headline");
-    const copy = teaserContent.querySelector(".teaser-copy");
-    const link = teaserContent.querySelector(".teaser-link");
 
-    if (path && circle) {
+    if (path && circle && teaserContent) {
       const pathLength = path.getTotalLength();
+      const startPoint = path.getPointAtLength(0);
 
-      const teaserPoints = [
+      // Set the initial position of the circle to the start of the path
+      gsap.set(circle, {
+        x: startPoint.x,
+        y: startPoint.y
+      });
+
+      // Define the positions of the teaser dots as a percentage of the total path length
+      const teaserDots = [
         {
-          progress: 0.2,
-          headline: "Entwicklungen Automobilmarkt",
-          copy: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.Ut enim.",
-          linkText: "Mehr erfahren →",
-          linkHref: "#link1"
-        },
-        {
-          progress: 0.4,
-          headline: "Headline 2",
-          copy: "Dies ist ein anderer erster Satz.<br>Ein weiterer Satz.<br>Noch ein Satz.",
-          linkText: "Details →",
-          linkHref: "#link2"
-        },
-        {
-          progress: 0.5,
-          headline: "Headline 3",
-          copy: "Ein neuer erster Satz.<br>Ein neuer zweiter Satz.<br>Ein neuer dritter Satz.",
-          linkText: "Weiterlesen →",
-          linkHref: "#link3"
+          position: 0.3,
+          headline: "Teaser 1 Headline",
+          copy: "Teaser 1 Inhalt",
+          cta: "CTA Text Einfügen ->"
         }
+        // {
+        //   position: 0.5,
+        //   headline: "Teaser 2 Headline",
+        //   copy: "Teaser 2 Inhalt",
+        //   cta: "Mehr erfahren ->"
+        // },
+        // {
+        //   position: 0.7,
+        //   headline: "Teaser 3 Headline",
+        //   copy: "Teaser 3 Inhalt",
+        //   cta: "Jetzt entdecken ->"
+        // }
       ];
 
-      const animation = gsap.to(circle, {
+      // Animate the Userdot along the path
+      gsap.to(circle, {
         scrollTrigger: {
           trigger: path,
           start: "top top",
-          end: "bottom bottom",
-          scrub: 1,
-          pin: true,
-          onUpdate: (self) => {
-            const progress = self.progress;
-            updateTeaser(progress);
-          }
+          end: "bottom",
+          scrub: true,
+          pin: false
         },
         motionPath: {
           path: path,
           align: path,
-          autoRotate: true,
+          autoRotate: false,
           alignOrigin: [0.5, 0.5]
         }
       });
 
-      gsap.ticker.add(() => {
-        const circlePosition = circle.getBoundingClientRect();
-        teaserContent.style.transform = `translate(${circlePosition.x}px, ${circlePosition.y}px)`;
-      });
+      teaserDots.forEach((dot) => {
+        const dotPosition = path.getPointAtLength(dot.position * pathLength);
 
-      function updateTeaser(progress) {
-        let teaserDisplayed = false;
-        teaserPoints.forEach((point) => {
-          if (Math.abs(progress - point.progress) < 0.2) {
-            headline.textContent = point.headline;
-            copy.innerHTML = point.copy;
-            link.textContent = point.linkText;
-            link.href = point.linkHref;
-            gsap.to(teaserContent, {
-              duration: 0.3,
-              opacity: 1
-            });
-            teaserDisplayed = true;
+        // Create a teaser dot in the SVG
+        const teaserdot = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "circle"
+        );
+        teaserdot.setAttribute("cx", dotPosition.x);
+        teaserdot.setAttribute("cy", dotPosition.y);
+        teaserdot.setAttribute("r", 40);
+        teaserdot.setAttribute("fill", "#2E908B");
+        path.parentNode.appendChild(teaserdot);
+
+        // Create a ScrollTrigger for each teaser dot
+        ScrollTrigger.create({
+          trigger: teaserdot,
+          start: "top center",
+          end: "bottom center",
+          onEnter: () => {
+            teaserContent.style.top = `${dotPosition.y}px`;
+            teaserContent.style.left = `${dotPosition.x}px`;
+            teaserContent.querySelector(".headline").textContent = dot.headline;
+            teaserContent.querySelector(".copy").textContent = dot.copy;
+            teaserContent.querySelector(".cta").textContent = dot.cta;
+
+            gsap.fromTo(
+              teaserContent,
+              { opacity: 0, scale: 0.1 },
+              {
+                opacity: 1,
+                scale: 1,
+                duration: 0.75
+              }
+            );
+          },
+          onLeave: () => {
+            gsap.to(teaserContent, { opacity: 0, duration: 0.75, scale: 0.1 });
+          },
+          onEnterBack: () => {
+            teaserContent.style.top = `${dotPosition.y}px`;
+            teaserContent.style.left = `${dotPosition.x}px`;
+            teaserContent.querySelector(".headline").textContent = dot.headline;
+            teaserContent.querySelector(".copy").textContent = dot.copy;
+            teaserContent.querySelector(".cta").textContent = dot.cta;
+
+            gsap.fromTo(
+              teaserContent,
+              { opacity: 0, scale: 0.1 },
+              {
+                opacity: 1,
+                scale: 1,
+                duration: 0.75
+              }
+            );
+          },
+          onLeaveBack: () => {
+            gsap.to(teaserContent, { opacity: 0, duration: 0.75 });
           }
         });
-
-        if (!teaserDisplayed) {
-          gsap.to(teaserContent, {
-            duration: 0.3,
-            opacity: 0
-          });
-        }
-      }
+      });
+    } else {
+      console.error("SVG path, circle, or teaser content not found");
     }
   });
 </script>
 
 <svg
-  width="974"
-  height="1891"
-  viewBox="0 0 974 1891"
-  fill="#53ABA4"
+  width="989"
+  height="3469"
+  viewBox="0 0 989 3469"
+  fill="none"
   xmlns="http://www.w3.org/2000/svg"
 >
   <path
     id="line"
-    d="M66 40L66 547.105C66.0565 650.334 149.758 734 253 734H747C850.277 734 934 817.723 934 921V1259C934 1352.89 857.441 1429 763 1429H556C462.112 1429 386 1352.44 386 1258V1067C386 972.559 309.441 896 215 896C120.559 896 44 972.559 44 1067L40.5 1686.5V1687.5C40.5 1781.94 120.559 1851 215 1851H640"
-    stroke="#53ABA4"
+    d="M504 40C504 98.72 551.78 146.5 610.5 146.5H753.5C806.71 146.5 850 189.34 850 242C850 294.66 806.71 337.5 753.5 337.5H246.5C132.64 337.5 40 429.69 40 543C40 656.31 132.64 750.5 246.5 750.5H510.5C587.42 750.5 650 813.08 650 890V1454C650 1531.47 587.42 1594.5 510.5 1594.5H246.5C209.59 1594.5 174.87 1608.95 148.73 1635.19C122.6 1661.42 108.13 1696.34 108 1733.5C108 1814 168.684 1872.5 248.5 1872.5H757.5C833.87 1872.5 896 1934.63 896 2011C896 2087.37 833.87 2149.5 757.5 2149.5H709.5C644.71 2149.5 592 2202.66 592 2268C591.46 2333.43 538.06 2386.5 472.5 2386.5H228.5C128.97 2386.5 48 2467.47 48 2567C48 2666.53 128.97 2747.5 228.5 2747.5H810.5C886.87 2747.5 949 2809.63 949 2886C949 2962.37 886.87 3024.5 810.5 3024.5H229.5C147.07 3024.5 80 3091.12 80 3173C80 3254.88 147.07 3321.5 229.5 3321.5L372 3321.52H396.5C454.85 3322.59 502 3370.4 502 3429"
+    stroke="##116864"
     stroke-width="80"
     stroke-linecap="round"
   />
   <g id="movingCircle">
-    <circle cx="10" cy="1679" r="40" fill="#2E908B" />
+    <circle cx="0" cy="0" r="40" fill="#2E908B" />
   </g>
 </svg>
 
 <div id="teaserContent" class="teaser-content">
-  <h2 class="teaser-headline">Headline</h2>
-  <p class="teaser-copy">
-    Dies ist der erste Satz des Textes.<br />
-    Dies ist der zweite Satz des Textes.<br />
-    Dies ist der dritte Satz des Textes.
-  </p>
-  <a href="#" class="teaser-link">
-    Mehr erfahren <span class="arrow">→</span>
-  </a>
+  <div class="headline"></div>
+  <div class="copy"></div>
+  <div class="cta"></div>
 </div>
 
 <style>
   .teaser-content {
     display: flex;
-    width: 376px;
-    height: auto;
     padding: var(--l, 32px);
     flex-direction: column;
     align-items: flex-start;
@@ -136,47 +162,42 @@
     align-self: stretch;
     border-radius: var(--m, 16px);
     background: var(--primary-bosch-trkis-50-markt-und-trends, #18837e);
-    color: #fff;
-    text-align: left;
     opacity: 0;
-    height: auto;
-    pointer-events: none;
-    position: absolute; /* Use absolute positioning */
+    position: absolute;
+    transform: translate(-50%, -50%);
     z-index: 1000;
-    transition: opacity 0.3s ease-in-out;
-  }
-
-  .teaser-headline {
-    font-size: 24px;
-    font-weight: bold;
-    line-height: 32px;
-    letter-spacing: -0.24px;
-    margin: 0;
-  }
-
-  .teaser-copy {
-    font-size: 14px;
-    margin: 0;
-    line-height: 1.5;
-  }
-
-  .teaser-link {
-    font-size: 14px;
-    font-weight: bold;
-    text-transform: uppercase;
+    pointer-events: none;
     color: #fff;
-    text-decoration: none;
-    display: flex;
-    align-items: center;
-    gap: 8px;
+    transform-origin: center center;
   }
 
-  .teaser-link .arrow {
-    font-size: 18px;
-    transition: transform 0.2s;
+  .headline {
+    color: #fff;
+    font-family: "Helvetica", sans-serif;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 32px; /* 133.333% */
+    letter-spacing: -0.24px;
   }
 
-  .teaser-link:hover .arrow {
-    transform: translateX(5px);
+  .copy {
+    color: #fff;
+    font-family: "Helvetica", sans-serif;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 150%; /* 21px */
+  }
+
+  .cta {
+    color: #fff;
+    font-family: "Helvetica", sans-serif;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 150%; /* 21px */
+    letter-spacing: 0.56px;
+    text-transform: uppercase;
   }
 </style>
